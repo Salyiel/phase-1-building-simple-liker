566a83ee4aecea20420bb82a83d4bba1f4ba32cc
Stacy <salyiela@icloud.com>
2024-04-12 21:13:05 +0300

    Completed lab assignment

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const error = document.querySelector('.hidden');
+const hearts = document.querySelectorAll('.like-glyph');
+
+hearts.forEach(heart => heart.addEventListener("click", servResponse ))
 
 
+function servResponse(hearts) {
+  mimicServerCall()
+  .then(() => {
+    if(hearts.target.textContent === EMPTY_HEART){
+      hearts.target.textContent = FULL_HEART
+      hearts.target.classList.add('activated-heart')
+    }
+    else if (hearts.target.textContent === FULL_HEART){
+      hearts.target.textContent = EMPTY_HEART
+    }
+  })
+  .catch(() => {
+    error.classList.remove('#hidden')
+    error.textContent = "error occured"
 
+    setTimeout(() => {
+      error.classList = "hidden"}, 3000)
+    })
+  }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
